3f04d8996a3f6e9e50736668d900d7b95444cdf0
Daniel Chong <danchong625@gmail.com>
2018-02-26 20:40:32 -0800

    [feat] initializes react with redux and persist

---
+++ src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { ConnectedRouter } from 'react-router-redux';
+
+import { configureStore, history } from './store/configureStore';
+
+import './global.css';
+
+import App from './components/app/app.jsx';
+
+const { store, persistor } = configureStore();
+
+render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
+);

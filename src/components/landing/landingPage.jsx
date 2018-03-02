a1c1105d6aa4d19541585329ebfa6001bf69034c
Gabriel Katz <gabriel.katz223@gmail.com>
2018-03-02 11:36:27 -0800

    [feat] adds basic formatting to calendar

--- src/components/landing/landingPage.jsx
+++ src/components/landing/landingPage.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 
 class LandingPage extends Component {
@@ -9,31 +10,36 @@ class LandingPage extends Component {
     super();
     BigCalendar.momentLocalizer(moment);
     this.state = {
-      eventsList: []
+      eventsList: [],
     }
   }
 
   componentDidMount() {
-    this.setState({
-      eventsList: [{
+    this.setState({     
         id: 0,
         title: 'All Day Event very long title',
         allDay: true,
-        start: new Date(2018, 3, 2),
-        end: new Date(2018, 3, 3),
-      }]
-    })
+        start: new Date(2018, 2, 2),
+        end: new Date(2018, 2, 3),
+      })
   }
 
 
   render() {
+    
     return (
       <div>
-        {console.log(this)}
         <BigCalendar
           events={this.state.eventsList}
+          defaultView={'day'}
+          step={15}
+          views={['week', 'day']}
           startAccessor='startDate'
           endAccessor='endDate'
+          min={new Date('2018-03-02T16:00:00.113Z')}
+          max={new Date('2018-03-02T04:00:00.113Z')}
+          showMultiDayTimes
+          defaultDate={new Date(2018, 2, 2)}
         />
       </div>
     )

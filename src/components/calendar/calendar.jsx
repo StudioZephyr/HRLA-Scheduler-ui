8560e5acb110a5fb371e984592868af8ad699e15
Gabriel Katz <gabriel.katz223@gmail.com>
2018-03-02 14:07:06 -0800

    [ops] Moves calendar logic to calendar compenent, and fixes calendar start/ end date accessor

---
+++ src/components/calendar/calendar.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+
+class LandingPage extends Component {
+  constructor() {
+    super();
+    BigCalendar.momentLocalizer(moment);
+    this.state = {
+      eventsList: [{
+        title: 'All Day Event very long initial event',
+        // allDay: true,
+        start: new Date(2018, 2, 2, 15, 0, 0),
+        end: new Date(2018, 2, 2, 17, 0, 0),
+      }],
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      eventsList: [{
+        title: 'Test event',
+        start: new Date(2018, 2, 2, 13, 0, 0),
+        end: new Date(2018, 2, 2, 15, 0, 0),
+        
+      }]
+    })
+  }
+
+
+  render() {
+
+    return (
+      <div>
+        <BigCalendar
+          events={this.state.eventsList}
+          defaultView={'day'}
+          step={15}
+          views={['week', 'day']}
+          min={new Date('2018-03-02T16:00:00.113Z')}
+          max={new Date('2018-03-02T04:00:00.113Z')}
+          defaultDate={new Date(2018, 2, 2)}
+        />
+      </div>
+    )
+  }
+};
+
+export default LandingPage;
--- src/components/landing/landingPage.jsx
+++ src/components/landing/landingPage.jsx
@@ -1,46 +1,20 @@
 import React, { Component } from 'react';
 
-import BigCalendar from 'react-big-calendar';
-import moment from 'moment';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+
+
 
 
 class LandingPage extends Component {
   constructor() {
     super();
-    BigCalendar.momentLocalizer(moment);
-    this.state = {
-      eventsList: [],
-    }
   }
-
-  componentDidMount() {
-    this.setState({     
-        id: 0,
-        title: 'All Day Event very long title',
-        allDay: true,
-        start: new Date(2018, 2, 2),
-        end: new Date(2018, 2, 3),
-      })
-  }
-
 
   render() {
-    
+
     return (
       <div>
-        <BigCalendar
-          events={this.state.eventsList}
-          defaultView={'day'}
-          step={15}
-          views={['week', 'day']}
-          startAccessor='startDate'
-          endAccessor='endDate'
-          min={new Date('2018-03-02T16:00:00.113Z')}
-          max={new Date('2018-03-02T04:00:00.113Z')}
-          showMultiDayTimes
-          defaultDate={new Date(2018, 2, 2)}
-        />
+        
       </div>
     )
   }

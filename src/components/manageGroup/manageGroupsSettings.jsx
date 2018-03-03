be124df3e166c96684233590edfa73f0ec61cef7
Daniel Chong <danchong625@gmail.com>
2018-03-03 13:01:58 -0800

    [feat] initializes manage groups settings for admin

---
+++ src/components/manageGroup/manageGroupsSettings.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class ManageGroupsSettings extends Component {
+  constructor(props) {
+    props(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Admin Manage Group View
+      </div>
+    )
+  }
+};
+
+export default ManageGroupsSettings;

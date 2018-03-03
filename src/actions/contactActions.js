ebafe0587c0117b93b3a8bd4cfca4fa86f8eb209
Daniel Chong <danchong625@gmail.com>
2018-03-03 00:18:39 -0800

    [ops] removes logs

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -5,7 +5,6 @@ const API_SERVER = process.env.API_SERVER;
 const getContacts = (id) => (dispatch) => {
   axios.get(`${API_SERVER}/api/contact/${id}`)
     .then(({ data }) => {
-      console.log(`DATA FROM GET CONTACTS: ${data}`);
       dispatch({ type: 'CONTACTS_GET_SUCCESS', payload: data.result });
     })
     .catch(err => {

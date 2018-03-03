654781e172e6d705ba8974d804e1ebe034e03763
Daniel Chong <danchong625@gmail.com>
2018-03-02 23:54:22 -0800

    [feat] implements redux and adds conditional to get contacts

--- src/components/contactSettings/contactSettings.jsx
+++ src/components/contactSettings/contactSettings.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { getContacts } from '../../actions/contactActions';
 
 class ContactSetting extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   render() {
+    const { contacts, getContacts, id, updated } = this.props;
+
+    if (!updated) {
+      getContacts(id);
+      return (
+        <div>
+          Updating...
+        </div>
+      )
+    }
+
+    console.log(contacts);
+
     return (
       <div>
         Contacts settings
@@ -14,4 +31,18 @@ class ContactSetting extends Component {
   }
 };
 
-export default ContactSetting;
+const ContactState = (state) => {
+  return {
+    contacts: state.contact.contacts,
+    id: state.auth.user.id,
+    updated: state.contact.contactsUpdated,
+  }
+};
+
+const ContactDispatch = (dispatch) => {
+  return {
+    getContacts: bindActionCreators(getContacts, dispatch),
+  }
+};
+
+export default connect(ContactState, ContactDispatch)(ContactSetting);

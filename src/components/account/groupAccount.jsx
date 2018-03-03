f3c3e01374ef060905069819bcf6bcb8da915fb3
Daniel Chong <danchong625@gmail.com>
2018-03-02 18:48:11 -0800

    [feat] implements basic groupName input and edit options

--- src/components/account/groupAccount.jsx
+++ src/components/account/groupAccount.jsx
@@ -5,14 +5,51 @@ class GroupAccount extends Component {
     super(props);
 
     this.state = {
-      
+      groupName: '',
+      editDisabled: true,
     }
   }
 
+  setGroupName(text) {
+    this.setState({
+      groupName: text,
+    });
+  }
+
+  toggleEdit() {
+    this.setState({
+      editDisabled: !this.state.editDisabled,
+    });
+  }
+
   render() {
+    const { updateLogin, user } = this.props;
+    const { groupName, editDisabled } = this.state;
+
     return (
       <div>
-        Group Account View
+        <form>
+          <input type="text" placeholder={user.groupName} disabled={editDisabled} onChange={(e) => {
+            this.setGroupName(e.target.value);
+          }} />
+          {
+            !editDisabled && 
+            <button onClick={(e) => {
+              e.preventDefault();
+              updateLogin({ groupName }, user.id);
+            }} >
+              SUBMIT
+            </button>
+          }
+          <button onClick={(e) => {
+            e.preventDefault();
+            this.toggleEdit();
+          }} >
+            {
+              editDisabled ? 'EDIT' : 'CANCEL'
+            }
+          </button>
+        </form>
       </div>
     )
   }

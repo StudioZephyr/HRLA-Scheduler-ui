7960df40ecefa76bb03dc14d8cd638ded9fa2700
Daniel Chong <danchong625@gmail.com>
2018-03-02 17:34:11 -0800

    [feat] initializes admin and group account settings components

---
+++ src/components/account/adminAccount.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class AdminAccount extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Admin Account View
+      </div>
+    )
+  }
+};
+
+export default AdminAccount;
---
+++ src/components/account/groupAccount.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class GroupAccount extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        Group Account View
+      </div>
+    )
+  }
+};
+
+export default GroupAccount;

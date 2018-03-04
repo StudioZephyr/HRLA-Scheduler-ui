ee0188800f623e6338ead210aac8c024b4d7cbcd
Daniel Chong <danchong625@gmail.com>
2018-03-03 22:04:41 -0800

    [feat] adds group view and state to get/keep/update logins

--- src/components/manageGroup/manageGroupsSettings.jsx
+++ src/components/manageGroup/manageGroupsSettings.jsx
@@ -1,14 +1,88 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import ManageGroupView from './manageGroupView.jsx';
 
 class ManageGroupsSettings extends Component {
   constructor(props) {
-    props(props);
+    super(props);
+
+    this.state = {
+      updated: false,
+      logins: [],
+    };
+
+    this.updateLogin = this.updateLogin.bind(this);
+  }
+
+  componentDidMount() {
+    const { updated } = this.state;
+
+    if (!updated) {
+      axios.get(`${process.env.API_SERVER}/api/login`)
+        .then(({ data }) => {
+          const loginsArr = data.result.sort((a, b) => {
+            return a.id - b.id;
+          });
+
+          this.setState({
+            logins: loginsArr,
+            updated: true,
+          });
+        })
+        .catch(err => {
+          console.log(`Error getting all Logins. ${err.message}`);
+          this.setState({
+            updated: true,
+          });
+        });
+    }
+  }
+
+  updateLogin(loginObj, id) {
+    axios.put(`${process.env.API_SERVER}/api/login/${id}`, loginObj)
+      .then(({ data }) => {
+        alert(`User, ${data.result.login}, has been updated!`);
+        this.setState({
+          updated: false,
+        });
+        this.componentDidMount();
+      })
+      .catch(err => {
+        console.log(`Error updating User. ${err.message}`);
+        this.setState({
+          updated: false,
+        });
+        this.componentDidMount();
+      });
   }
 
   render() {
+    const { updated, logins } = this.state;
+
+    if (!updated) {
+      return (
+        <div>
+          Updating..
+        </div>
+      )
+    }
+
+    if (logins.length === 0) {
+      return (
+        <div>
+          Error getting logins. SOMETHING IS WRONG! AHHHHHHHHHHHHHHH
+        </div>
+      )
+    }
+
     return (
       <div>
-        Admin Manage Group View
+        {
+          logins.map((login, i) => (
+            <ManageGroupView key={`manage-group-${i}`} login={login} updateLogin={this.updateLogin} />
+          ))
+        }
       </div>
     )
   }

bd04e0b195fd846c5b01954c0c3d92037d301a2e
Daniel Chong <danchong625@gmail.com>
2018-03-03 22:39:46 -0800

    [feat] adds form and input state to add contact

--- src/components/manageGroup/addGroupView.jsx
+++ src/components/manageGroup/addGroupView.jsx
@@ -13,12 +13,75 @@ class AddGroupView extends Component {
     };
   }
 
+  setGroupName(text) {
+    this.setState({
+      groupName: text,
+    });
+  }
+
+  setLogin(text) {
+    this.setState({
+      login: text,
+    });
+  }
+
+  setPassword(text) {
+    this.setState({
+      password: text,
+    });
+  }
+
+  setType(text) {
+    this.setState({
+      type: text,
+    });
+  }
+
+  toggleAdd() {
+    this.setState({
+      add: !this.state.add,
+    });
+  }
+
   render() {
     const { addLogin } = this.props;
+    const { add, login, password, groupName, type } = this.state;
 
     return (
       <div>
-        HELLO ADDING
+        {
+          add &&
+          <form>
+            <input type="text" placeholder={'Login'} onChange={(e) => {
+              this.setLogin(e.target.value);
+            }} />
+            <input type="text" placeholder={'Password'} onChange={(e) => {
+              this.setPassword(e.target.value);
+            }} />
+            <input type="text" placeholder={'Group Name'} onChange={(e) => {
+              this.setGroupName(e.target.value);
+            }} />
+            <select onChange={(e) => {
+              this.setType(e.target.value);
+            }} >
+              <option value="" disabled selected>Choose Type</option>
+              <option value="admin">Admin</option>
+              <option value="group">Group</option>
+            </select>
+            <button onClick={(e) => {
+              e.preventDefault();
+              addLogin({ login, password, groupName, type });
+            }} >
+              SUBMIT
+            </button>
+          </form>
+        }
+        <button onClick={(e) => {
+          e.preventDefault();
+          this.toggleAdd();
+        }} >
+          { add ? 'CANCEL' : 'ADD' }
+        </button>
       </div>
     )
   }

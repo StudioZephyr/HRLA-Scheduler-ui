16317c4cff094b3c2b2cea619a2fa72ba4a7de35
Daniel Chong <danchong625@gmail.com>
2018-03-05 18:05:52 -0800

    [style] removes max-width on user settings forms

--- src/components/loginSettings/groupLogin.jsx
+++ src/components/loginSettings/groupLogin.jsx
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 
 const GroupLoginSettings = (props) => (
-  <div className="login-settings-form">
-    <form>
-      <div className="form-group">
-        <label htmlFor="group-login-groupName">Group Name</label>
-        <input type="text" id="group-login-groupName" className="form-control" placeholder={props.groupName} onChange={(e) => {
-          props.setGroupName(e.target.value);
-        }} disabled={props.editDisabled} />
-      </div>
-    </form>
-  </div>
+  <form>
+    <div className="form-group">
+      <label htmlFor="group-login-groupName">Group Name</label>
+      <input type="text" id="group-login-groupName" className="form-control" placeholder={props.groupName} onChange={(e) => {
+        props.setGroupName(e.target.value);
+      }} disabled={props.editDisabled} />
+    </div>
+  </form>
 );
 
 export default GroupLoginSettings;

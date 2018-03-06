2269273e85e49dd2e7cb4b494ebd0fc7e6f61b6a
Daniel Chong <danchong625@gmail.com>
2018-03-05 17:28:05 -0800

    [style] adds bootstrap and custom styling for admin login settings

--- src/components/loginSettings/adminLogin.jsx
+++ src/components/loginSettings/adminLogin.jsx
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 
 const AdminLoginSettings = ({ editDisabled, groupName, login, password, setGroupName, setLogin, setPassword }) => (
-  <form>
-    <input type="text" placeholder={groupName} disabled={editDisabled} onChange={(e) => {
-      setGroupName(e.target.value);
-    }} />
-    <input type="text" placeholder={login} disabled={editDisabled} onChange={(e) => {
-      setLogin(e.target.value);
-    }} />
-    <input type="text" placeholder={password} disabled={editDisabled} onChange={(e) => {
-      setPassword(e.target.value);
-    }} />
-  </form>
+  <div className="login-settings-form">
+    <form>
+      <div className="form-group">
+        <label htmlFor="admin-login-groupName">Group Name</label>
+        <input type="text" id="admin-login-groupName" className="form-control" placeholder={groupName} disabled={editDisabled} onChange={(e) => {
+          setGroupName(e.target.value);
+        }} />
+      </div>
+      <div className="form-group">
+        <label htmlFor="admin-login-login">Login</label>
+        <input type="text" id="admin-login-login" className="form-control" placeholder={login} disabled={editDisabled} onChange={(e) => {
+          setLogin(e.target.value);
+        }} />
+      </div>
+      <div className="form-group">
+        <label htmlFor="admin-login-pw">Password</label>
+        <input type="text" id="admin-login-pw" className="form-control" placeholder={password} disabled={editDisabled} onChange={(e) => {
+          setPassword(e.target.value);
+        }} />
+      </div>
+    </form>
+  </div>
 );
 
 export default AdminLoginSettings;

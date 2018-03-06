eb6db04a434d6608926e12a7740950a4efb0970f
Daniel Chong <danchong625@gmail.com>
2018-03-05 17:28:51 -0800

    [style] adds custom styling and bootstrap's grid layout

--- src/components/loginSettings/loginSettings.jsx
+++ src/components/loginSettings/loginSettings.jsx
@@ -4,6 +4,8 @@ import { bindActionCreators } from 'redux';
 
 import { updateLogin } from '../../actions/authActions';
 
+import './loginSettings.css';
+
 import GroupLogin from './groupLogin.jsx';
 import AdminLogin from './adminLogin.jsx';
 
@@ -54,36 +56,40 @@ class LoginSetting extends Component {
     const { editDisabled, groupName, login, password } = this.state;
 
     return (
-      <div>
-        {
-          user.type === 'group' &&
-          <GroupLogin groupName={groupName} setGroupName={this.setGroupName} editDisabled={editDisabled} />
-        }
-        {
-          user.type === 'admin' &&
-          <AdminLogin groupName={groupName} login={login} password={password}
-            setGroupName={this.setGroupName} 
-            setLogin={this.setLogin}
-            setPassword={this.setPassword}
-            editDisabled={editDisabled} 
-          />
-        }
-        {
-          !editDisabled &&
-          <button onClick={(e) => {
+      <div id="login-settings" className="row justify-content-center settings-row" >
+        <div className="col col-lg-auto">
+          {
+            user.type === 'group' &&
+            <GroupLogin groupName={groupName} setGroupName={this.setGroupName} editDisabled={editDisabled} />
+          }
+          {
+            user.type === 'admin' &&
+            <AdminLogin groupName={groupName} login={login} password={password}
+              setGroupName={this.setGroupName} 
+              setLogin={this.setLogin}
+              setPassword={this.setPassword}
+              editDisabled={editDisabled} 
+            />
+          }
+        </div>
+        <div className="col col-lg-4 align-self-end">
+          {
+            !editDisabled &&
+            <button className="btn btn-primary" onClick={(e) => {
+              e.preventDefault();
+              updateLogin({ groupName, login, password }, user.id);
+              this.toggleEdit();
+            }}>
+              SUBMIT
+            </button>
+          }
+          <button className="btn btn-primary" onClick={(e) => {
             e.preventDefault();
-            updateLogin({ groupName, login, password }, user.id);
             this.toggleEdit();
           }}>
-            SUBMIT
+            { editDisabled ? 'EDIT' : 'CANCEL' }
           </button>
-        }
-        <button onClick={(e) => {
-          e.preventDefault();
-          this.toggleEdit();
-        }}>
-          { editDisabled ? 'EDIT' : 'CANCEL' }
-        </button>
+        </div>
       </div>
     )
   }

94169e03aec2df970fbce3b0acd2878085b8efe4
Daniel Chong <danchong625@gmail.com>
2018-03-05 18:48:33 -0800

    [style] adds custom and bootstrap styling for forms to add

--- src/components/manageGroup/addGroupView.jsx
+++ src/components/manageGroup/addGroupView.jsx
@@ -48,40 +48,61 @@ class AddGroupView extends Component {
     const { add, login, password, groupName, type } = this.state;
 
     return (
-      <div>
-        {
-          add &&
-          <form>
-            <input type="text" placeholder={'Login'} onChange={(e) => {
-              this.setLogin(e.target.value);
-            }} />
-            <input type="text" placeholder={'Password'} onChange={(e) => {
-              this.setPassword(e.target.value);
-            }} />
-            <input type="text" placeholder={'Group Name'} onChange={(e) => {
-              this.setGroupName(e.target.value);
-            }} />
-            <select onChange={(e) => {
-              this.setType(e.target.value);
-            }} >
-              <option value="" disabled selected>Choose Type</option>
-              <option value="admin">Admin</option>
-              <option value="group">Group</option>
-            </select>
-            <button onClick={(e) => {
+      <div className="row justify-content-center manage-add-row">
+        <div className="col col-lg-auto">
+          {
+            add &&
+            <form>
+              <div className="form-row">
+                <div className="form-group manage-add-form">
+                  <label htmlFor="manage-add-login">Login</label>
+                  <input type="text" id="manage-add-login" className="form-control" placeholder={'Login'} onChange={(e) => {
+                    this.setLogin(e.target.value);
+                  }} />
+                </div>
+                <div className="form-group manage-add-form">
+                  <label htmlFor="manage-add-pw">Password</label>
+                  <input type="text" id="manage-add-pw" className="form-control" placeholder={'Password'} onChange={(e) => {
+                    this.setPassword(e.target.value);
+                  }} />
+                </div>
+                <div className="form-group manage-add-form">
+                  <label htmlFor="manage-add-groupName">Group Name</label>
+                  <input type="text" id="manage-add-groupName" className="form-control" placeholder={'Group Name'} onChange={(e) => {
+                    this.setGroupName(e.target.value);
+                  }} />
+                </div>
+                <div className="form-group manage-add-form">
+                  <label htmlFor="manage-add-type">Type</label>
+                  <select id="manage-add-type" className="form-control" onChange={(e) => {
+                    this.setType(e.target.value);
+                  }} >
+                    <option value="" disabled selected>Choose...</option>
+                    <option value="admin">Admin</option>
+                    <option value="group">Group</option>
+                  </select>
+                </div>
+              </div>
+            </form>
+          }
+        </div>
+        <div className="col col-lg-auto align-self-end">
+          {
+            add &&
+            <button className="btn btn-success contact-add-btn" onClick={(e) => {
               e.preventDefault();
               addLogin({ login, password, groupName, type });
             }} >
               SUBMIT
             </button>
-          </form>
-        }
-        <button onClick={(e) => {
-          e.preventDefault();
-          this.toggleAdd();
-        }} >
-          { add ? 'CANCEL' : 'ADD' }
-        </button>
+          }
+          <button className="btn btn-info contact-add-btn" onClick={(e) => {
+            e.preventDefault();
+            this.toggleAdd();
+          }} >
+            { add ? 'CANCEL' : 'ADD' }
+          </button>
+        </div>
       </div>
     )
   }

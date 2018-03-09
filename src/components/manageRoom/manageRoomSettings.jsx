242bb8e378110b55cd3bc2a994083689eaed33cd
Daniel Chong <danchong625@gmail.com>
2018-03-09 13:29:58 -0800

    [feat] adds retrieval of rooms on mount and implements loading

--- src/components/manageRoom/manageRoomSettings.jsx
+++ src/components/manageRoom/manageRoomSettings.jsx
@@ -8,11 +8,45 @@ const API_SERVER = process.env.API_SERVER;
 class ManageRoomsSettings extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      updated: false,
+      rooms: [],
+    };
+  }
+
+  componentDidMount() {
+    const { updated } = this.state;
+
+    if (!updated) {
+      axios.get(`${API_SERVER}/api/rooms`)
+        .then(({ data }) => {
+          this.setState({
+            rooms: data.result,
+            updated: true,
+          });
+        })
+        .catch(err => {
+          console.log(`Error getting Rooms. ${err.message}`);
+          alert(`There was an issue retrieving room information. Please refresh the page.`);
+          this.setState({
+            updated: true,
+          });
+        });
+    }
   }
 
   render() {
+    const { updated, rooms } = this.state;
+
+    if (!updated) {
+      return (
+        <Loading />
+      )
+    }
+
     return (
-      <div>
+      <div className="account-settings">
         room settings
       </div>
     )

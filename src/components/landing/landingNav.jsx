0b49de226f6f78364117a6dc2c3746697d51c3d9
Daniel Chong <danchong625@gmail.com>
2018-04-03 15:33:45 -0700

    [feat] initializes separate navbar for landing

---
+++ src/components/landing/landingNav.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class LandingNav extends Component {
+  constructor() {
+    super();
+  }
+
+  smoothScrollTo(key) {
+    document.querySelector(`#${key}`).scrollIntoView({ behavior: 'smooth' });
+  }
+
+  render() {
+    return (
+      <nav className="navbar fixed-top navbar-expand-lg bg-primary navbar-text-color">
+        <span className="navbar-brand">
+          <Link to="/">HRLA Scheduler</Link>
+        </span>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarAuthContent" aria-controls="navbarAuthContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse d-flex flex-row-reverse" id="navbarAuthContent">
+          <div className="navbar-nav">
+            <span className="nav-item nav-link">
+
+            </span>
+            <span className="nav-item nav-link">
+              <Link to="/login">Login</Link>
+            </span>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default LandingNav;
